b7079b19e8c304f6dd6d574053483a6fbe7127c0
Alan Dragicevich <alan@geneious.com>
2021-12-24 15:01:31 +1300

    Replaced test suite with Jest
    Replaced Mocha/Chai test suite with Jest.
    
    Some custom config for Jest added to support running tests for ES6 modules.

--- test.js
+++ test.js
@@ -1,57 +1,48 @@
-
-require('mocha');
-var should = require('should');
-var assert = require('chai').assert;
-
+import { parse_json, parse_newick } from "./src/index.js";
 
 describe('BioJS2 Tree Test', function () {
-
-	var parser = require('./src/newick');
-
-	console.log(parser);
-    var newick = "((human,chimp),mouse);";
-    var tree = parser.parse_newick(newick);
-		var backToNewick = parser.parse_json(tree);
+    const newick = "((human,chimp),mouse);";
+    const tree = parse_newick(newick);
+	const backToNewick = parse_json(tree);
 
     // Newick
-    describe ('Newick reader', function () {
-			it ("Exists and is called tree.parse_newick", function () {
-			    assert.isDefined(parser.parse_newick);
-			});
-
-			it ("Can read a simple tree", function () {
-			    assert.isDefined(tree);
-			});
-			it ("The returned tree has the correct structure", function () {
-			    assert.property(tree, "name");
-			    assert.property(tree, "children");
-			    assert.property(tree.children[0], "name");
-			    assert.property(tree.children[0], "children");
-			    assert.strictEqual(tree.children[0].children[0].name, "human");
-			    assert.notProperty(tree.children[0].children[0], "children");
-			});
-
-			it ("Reads the branch lenghts", function () {
-			    var newick = "((human:0.2,chimp:0.3),mouse:0.5);";
-			    var tree = parser.parse_newick(newick);
-			    assert.closeTo(tree.children[1].branch_length, 0.5, 0.05);
-			    assert.closeTo(tree.children[0].children[0].branch_length, 0.2, 0.05);
-			    assert.closeTo(tree.children[0].children[1].branch_length, 0.3, 0.05);
-			});
-    });
-
-		//JSON
-		describe ('JSON reader', function () {
-			it ("Exists and is called parser.parse_json", function () {
-					assert.isDefined(parser.parse_json);
-			});
-
-			it ("Can read a simple JSON tree", function () {
-					assert.isDefined(backToNewick);
-			});
-			it ("The returned tree has the correct structure", function () {
-					assert.strictEqual(backToNewick, newick);
-			});
+	describe('Newick reader', function () {
+		it("Exists and is called tree.parse_newick", function () {
+			expect(parse_newick).toBeDefined();
 		});
 
+		it("Can read a simple tree", function () {
+			expect(tree).toBeDefined();
+		});
+		it("The returned tree has the correct structure", function () {
+			expect(tree).toHaveProperty("name");
+			expect(tree).toHaveProperty("children");
+			expect(tree.children[0]).toHaveProperty("name");
+			expect(tree.children[0]).toHaveProperty("children");
+			expect(tree.children[0].children[0].name).toEqual("human");
+			expect(tree.children[0].children[0].children).toBeUndefined();
+		});
+
+		it("Reads the branch lengths", function () {
+			const newick = "((human:0.2,chimp:0.3),mouse:0.5);";
+			const tree = parse_newick(newick);
+			expect(tree.children[1].branch_length).toBeCloseTo(0.5, 0.05);
+			expect(tree.children[0].children[0].branch_length).toBeCloseTo(0.2, 0.05);
+			expect(tree.children[0].children[1].branch_length).toBeCloseTo(0.3, 0.05);
+		});
+    });
+
+	// JSON
+	describe('JSON reader', function () {
+		it("Exists and is called parse_json", function () {
+			expect(parse_json).toBeDefined();
+		});
+
+		it("Can read a simple JSON tree", function () {
+			expect(backToNewick).toBeDefined();
+		});
+		it("The returned tree has the correct structure", function () {
+			expect(backToNewick).toEqual(newick);
+		});
+	});
 });
